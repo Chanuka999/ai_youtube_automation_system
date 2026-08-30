7740324638a2ebe6f12940f2f717a93a5f31710c
chanuka randitha <chanukaranditha99@gmail.com>
2026-08-30 10:56:40 +0530

    feat:create youtbe controller connect to route page

--- backend/src/routes/youtube.routes.ts
+++ backend/src/routes/youtube.routes.ts
@@ -5,7 +5,7 @@ import {
   youtubeCallback,
   getYouTubeStatus,
 } from "../controllers/youtube.controller.js";
-
+import {uploadVideo} from "../controllers/youtubeUpload.controller.js";
 const router = Router();
 
 router.get("/connect", connectYouTube);
@@ -13,5 +13,5 @@ router.get("/connect", connectYouTube);
 router.get("/status", getYouTubeStatus);
 
 router.get("/callback", youtubeCallback);
-
+router.post("/upload",uploadVideo);
 export default router;
